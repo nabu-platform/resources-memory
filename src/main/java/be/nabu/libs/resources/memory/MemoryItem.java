f00204ea00ebc28914dd911389ab7a1897058962
alex <alex@nabu.be>
2015-11-12 08:00:49 +0100

    Added ability to set max size on memory item

--- src/main/java/be/nabu/libs/resources/memory/MemoryItem.java
+++ src/main/java/be/nabu/libs/resources/memory/MemoryItem.java
@@ -7,17 +7,25 @@ import be.nabu.libs.resources.api.FiniteResource;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.TimestampedResource;
 import be.nabu.libs.resources.api.WritableResource;
+import be.nabu.utils.io.IOUtils;
 import be.nabu.utils.io.api.ByteBuffer;
 import be.nabu.utils.io.api.ReadableContainer;
 import be.nabu.utils.io.api.WritableContainer;
+import be.nabu.utils.io.buffers.bytes.ByteBufferFactory;
 import be.nabu.utils.io.buffers.bytes.DynamicByteBuffer;
 
 public class MemoryItem extends MemoryResource implements ReadableResource, WritableResource, FiniteResource, TimestampedResource {
 
 	private DynamicByteBuffer container = new DynamicByteBuffer();
+	private long maxSize;
 
 	public MemoryItem(String name) {
+		this(name, 0);
+	}
+	
+	public MemoryItem(String name, long maxSize) {
 		super(name);
+		this.maxSize = maxSize;
 		container.mark();
 	}
 	
@@ -27,7 +35,10 @@ public class MemoryItem extends MemoryResource implements ReadableResource, Writ
 		container.reopen();
 		// truncate all the data inside of it
 		container.truncate();
-		return container;
+		WritableContainer<ByteBuffer> target = maxSize > 0
+			? IOUtils.limitWritable(container, maxSize)
+			: container;
+		return IOUtils.bufferWritable(target, ByteBufferFactory.getInstance().newInstance(4096, true));
 	}
 
 	@Override
