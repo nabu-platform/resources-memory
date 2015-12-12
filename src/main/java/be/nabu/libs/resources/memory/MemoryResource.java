0ac47d01e67ef66ff24f227a9aa67eaea91bf2c7
alex <alex@nabu.be>
2015-12-12 12:33:07 +0100

    Added stringification

--- src/main/java/be/nabu/libs/resources/memory/MemoryResource.java
+++ src/main/java/be/nabu/libs/resources/memory/MemoryResource.java
@@ -57,4 +57,9 @@ public class MemoryResource implements Resource, Closeable, LocatableResource {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	@Override
+	public String toString() {
+		return getURI().toString();
+	}
 }
